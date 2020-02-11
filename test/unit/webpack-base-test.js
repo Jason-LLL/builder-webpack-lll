ed60067cb90a53c8b4e0252375e328f65434ad6a
YueLu <yuelu@YueLudeMacBook-Pro.local>
2020-02-11 23:12:17 +0800

    feat: build travis ci fix unit bug

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack-base unit test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     //console.log(baseConfig);
     it('entry', () => {
-        assert.equal(baseConfig.entry.index,'/Users/yuelu/Desktop/webpack/webpackDemo/builder-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.hiwebpack,'/Users/yuelu/Desktop/webpack/webpackDemo/builder-webpack/test/smoke/template/src/hiwebpack/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack-lll/test/smoke/template/src/index/index.js') > -1);
+        assert.equal(baseConfig.entry.hiwebpack.indexOf('builder-webpack-lll/test/smoke/template/src/index/index.js') > -1);
     })
 })
