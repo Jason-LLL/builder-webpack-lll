49895ff2c1487a05bf3b0d67a857a0a8c4817a6f
YueLu <yuelu@YueLudeMacBook-Pro.local>
2020-02-12 09:51:45 +0800

    feat: build travis ci fix assert

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack-base unit test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     //console.log(baseConfig);
     it('entry', () => {
-        assert.equal(baseConfig.entry.index.indexOf('builder-webpack-lll/test/smoke/template/src/index/index.js') > -1);
-        assert.equal(baseConfig.entry.hiwebpack.indexOf('builder-webpack-lll/test/smoke/template/src/index/index.js') > -1);
+        assert.ok(baseConfig.entry.index.indexOf('builder-webpack-lll/test/smoke/template/src/index/index.js') > -1);
+        assert.ok(baseConfig.entry.hiwebpack.indexOf('builder-webpack-lll/test/smoke/template/src/index/index.js') > -1);
     })
 })
